41dcc65faf23147c5910b61696da77f739d697f7
DamoM73 <damomurtagh@gmail.com>
2021-02-15 22:28:04 +1000

    Stop on spot

---
+++ stop_at_distance/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+ultra = UltrasonicSensor(Port.S4)
+left_motor = Motor(Port.C)
+right_motor = Motor(Port.B)
+robot = DriveBase(left_motor,right_motor,55,142)
+
+# Write your program here.
+
+
+
+# ---- MAIN PROGRAM ----
+while True:
+    dist = ultra.distance()
+    ev3.screen.clear()
+    ev3.screen.draw_text(80,50,str(dist))
+    if dist > 100:
+        robot.drive(1000,0)
+    else:
+        robot.stop()
+
